23582162b951f1a0ebdd917a8a4efd78204c4ece
baki <bbaki9919@gmail.com>
2024-03-06 11:37:30 +0100

    database specification readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-demo
+# Description
+Ce projet permet d'effectue les opérations CRUD sur des membres.
+
+Pour utiliser le projet, il faut créer une base de données mariadb nommé membre, puis y insere le
+contenu du fichier jeux_donnees.sql qui se trouve dans le package database.
+
+# Spécification : http://localhost:5000/swagger-ui/index.html
+
+# Auteur :
+BERTE Bakary, ABASY Nael, AIT M'HAMED aimad
---
+++ src/main/java/com/database/jeux_donnees.sql
@@ -0,0 +1,9 @@
+-- Insertion des membres
+
+INSERT INTO `membre` (`id`, `adresse`, `date_naissance`, `mail`, `mot_de_passe`, `nom`, `prenom`) VALUES (NULL, '2 Rue des archives', '2014-04-01', 'killer@gmail.com', 'killer', 'Zoldyc', 'Kirua');
+
+INSERT INTO `membre` (`id`, `adresse`, `date_naissance`, `mail`, `mot_de_passe`, `nom`, `prenom`) VALUES (NULL, '5 Rue Pasteur', '2010-05-12', 'leo@gmail.com', 'lion', 'Leo', 'King');
+
+INSERT INTO `membre` (`id`, `adresse`, `date_naissance`, `mail`, `mot_de_passe`, `nom`, `prenom`) VALUES (NULL, '15 Rue Lanredec', '2009-04-20', 'tana@gmail.com', 'tana', 'Tidiane', 'Ahmed');
+
+INSERT INTO `membre` (`id`, `adresse`, `date_naissance`, `mail`, `mot_de_passe`, `nom`, `prenom`) VALUES (NULL, '3 Rue George', '2012-09-18', 'nara@gmail.com', 'nara', 'Diabate', 'Ahmed');
